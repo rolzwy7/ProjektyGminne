6bf6ad63539565cf8a921dcd7550f7775b6c334d
rolzwy7 <rolzwy7@gmail.com>
2019-05-21 15:54:30 +0200

    update

--- ProjektyGminne/ProjektyGminne/admin.py
+++ ProjektyGminne/ProjektyGminne/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 
 class ProjektyGminneAdminSite(admin.AdminSite):
-    site_header = "Projekty Gminne - Administrator"
-    index_template = "admin/index_custom.html"
-    login_template = "admin/login_custom.html"
+    site_header = "Projekty Gminne - Warszawa"
+    index_template = "admin/base/index.html"
+    login_template = "admin/base/login.html"
 
 
 admin_site = ProjektyGminneAdminSite(name="projekty-gminne-admin-site")
